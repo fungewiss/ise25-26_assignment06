5f358cd5538cf1e16b48cf6f1581cf31e70d731f
fabian271828 <fabian.ungewiss@gmx.de>
2025-12-02 17:54:01 +0100

    BP1: impl. user representation and mapper

--- api/src/main/java/de/seuhd/campuscoffee/api/mapper/UserDtoMapper.java
+++ api/src/main/java/de/seuhd/campuscoffee/api/mapper/UserDtoMapper.java
@@ -1,5 +1,15 @@
 package de.seuhd.campuscoffee.api.mapper;
 
+import de.seuhd.campuscoffee.api.dtos.UserDto;
+import de.seuhd.campuscoffee.domain.model.User;
+import org.mapstruct.Mapper;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+
+@ConditionalOnMissingBean // prevent IntelliJ warning about duplicate beans
+@Mapper(componentModel = "spring")
 public interface UserDtoMapper {
     //TODO: Implement user DTO mapper
+
+    UserDto fromDomain(User source);
+    User toDomain(UserDto source);
 }
