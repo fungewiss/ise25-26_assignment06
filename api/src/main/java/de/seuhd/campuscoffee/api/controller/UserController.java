c9b757e70f26fcac2aea276bdbca55c5c9a9013d
fabian271828 <fabian.ungewiss@gmx.de>
2025-12-02 20:30:47 +0100

    BP2: impl. UserController. Does not yet compile

--- api/src/main/java/de/seuhd/campuscoffee/api/controller/UserController.java
+++ api/src/main/java/de/seuhd/campuscoffee/api/controller/UserController.java
@@ -5,6 +5,16 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import jakarta.validation.Valid;
+
+import de.seuhd.campuscoffee.domain.ports.UserService;
+import de.seuhd.campuscoffee.api.mapper.UserDtoMapper;
+import de.seuhd.campuscoffee.api.dtos.UserDto;
+import de.seuhd.campuscoffee.domain.model.User;
+
+ipmort java.util.List;
+
 
 @Tag(name = "Users", description = "Operations related to user management.")
 @Controller
@@ -13,4 +23,72 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class UserController {
     //TODO: Implement user controller
-}
+
+    private final UserService userService;
+    private final UserDtoMapper userDtoMapper;
+
+    @GetMapping("")
+    public ResponseEntity<List<UserDto>> getAllUsers() {
+        return ResponseEntity.ok(
+            userService.getAllUsers().stream()
+                .map(userDtoMapper::fromDomain)
+                .toList()
+        );
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable Long id) {
+        return ResponseEntity.ok(
+            userDtoMapper.fromDomain(
+                userService.getUserById(id)
+            )
+        );
+    }
+
+    @GetMapping("/filter")
+    public ResponseEntity<List<UserDto>> filter(
+        @RequestParam("loginName") String loginName) {
+        return ResponseEntity.ok(
+            userDtoMapper.fromDomain(
+                userService.getUserByLoginName(loginName)
+            )
+        );
+    }
+
+    @PostMapping("")
+    public ResponseEntity<UserDto> createUser(@RequestBody @Valid UserDto userDto) {
+        User createdUser = upsertUser(userDto);
+        return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(createdUser.id())
+                .toUri();
+                .body(createdUser);
+        );
+    }    
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDto> updateUser(
+        @PathVariable Long id,
+        @RequestBody @Valid UserDto userDto) {
+            if (!id.equals(userDto.id())) {
+                throw new IllegalArgumentException("User ID in path and body do not match.");
+            }
+            return ResponseEntity.ok(
+                upsertUser(userDto)
+            );
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    private User upsertUser(UserDto userDto) {
+        return userDtoMapper.fromDomain(
+            userService.upsertUser(
+                userDtoMapper.toDomain(userDto)
+            )
+        );
+    }
+}    
